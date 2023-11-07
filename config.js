c4392eeecd446da793d8be95242efe5f9c8e9093
M <61662338+ZombiePlugin@users.noreply.github.com>
2023-11-07 18:39:46 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://watch-movies.muzameelahmed1.workers.dev",
+  VITE_TMDB_READ_API_KEY:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjk3MjJlOTAxYmIzMjE1MjUwZDM1NjBiNWYwZDIwZiIsInN1YiI6IjY1NGE4MmEwNmJlYWVhMDBlYWY5YWRmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.csjWkwWCxCvoGQ-QR0Rbvy4KC2AaqOR_hgj-UYyXR38",
+};
